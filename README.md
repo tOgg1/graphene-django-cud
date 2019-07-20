5a997a48fce48ad3de9c81223fd00cd623294b59
Tormod Haugland <tormod.haugland@gmail.com>
2019-07-20 19:56:07 +0200

    Add build configuration

---
+++ README.md
@@ -0,0 +1,2 @@
+# Graphene Django CUD
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graphene-django-cud",
+    version="0.0.1",
+    author="Tormod Haugland",
+    author_email="tormod.haugland@gmail.com",
+    description="Create, update and delete mutations for graphene-django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/t.-haugland-consulting-as/graphene-django-cud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programing Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
