0d7ecd32f453ac1c412ab8abf74f76cf36d2baa5
Tormod Haugland <tormod.haugland@gmail.com>
2020-01-11 15:29:29 +0100

    Move graphql lexer into conf file

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,11 @@
 import sys
 from os.path import dirname, join, abspath
 
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+from sphinx.highlighting import lexers
+
 project = 'graphene-django-cud'
 copyright = '2019, Tormod Haugland'
 author = 'Tormod Haugland'
@@ -26,17 +31,13 @@ author = 'Tormod Haugland'
 # The full version, including alpha/beta/rc tags
 release = '0.2.2'
 
-# Inlcude extensions
-sys.path.append(abspath(join(dirname(__file__), "_ext")))
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "sphinx_rtd_theme",
-        "graphql_lexer"
+        "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,3 +62,44 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+# Add custom graphql lexer
+
+# -*- coding: utf-8 -*-
+"""
+    pygments.lexers.graphql
+    ~~~~~~~~~~~~~~~~~~~~
+    Lexers for GraphQL formats.
+    :copyright: Copyright 2017 by Martin Zlámal.
+    :license: BSD, see LICENSE for details.
+"""
+
+
+class GraphqlLexer(RegexLexer):
+    """
+    Lexer for GraphQL.
+    """
+
+    name = 'GraphQL'
+    aliases = ['graphql', 'gql']
+    filenames = ['*.graphql', '*.gql']
+    mimetypes = ['application/graphql']
+
+    tokens = {
+        'root': [
+            (r'#.*', Comment.Singline),
+            (r'\.\.\.', Operator),
+            (r'"([^\\"]|\\")*"', String.Double),
+            (r'(-?0|-?[1-9][0-9]*)(\.[0-9]+[eE][+-]?[0-9]+|\.[0-9]+|[eE][+-]?[0-9]+)', Number.Float),
+            (r'(-?0|-?[1-9][0-9]*)', Number.Integer),
+            (r'\$+[_A-Za-z][_0-9A-Za-z]*', Name.Variable),
+            (r'[_A-Za-z][_0-9A-Za-z]+\s?:', Text),
+            (r'(type|query|fragment|mutation|@[a-z]+|on|true|false|null)\b', Keyword.Type),
+            (r'[!$():=@\[\]{|}]+?', Punctuation),
+            (r'[_A-Za-z][_0-9A-Za-z]*', Keyword),
+            (r'(\s|,)', Text),
+        ]
+    }
+
+
+lexers["graphql"] = GraphqlLexer()
