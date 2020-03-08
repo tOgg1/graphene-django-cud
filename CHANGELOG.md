ee03992c7c623e0bb440ed682489a4daac8892c1
Tormod Haugland <tormod.haugland@gmail.com>
2020-03-08 17:27:47 +0100

    Bumpversion 0.3.1 → 0.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## Version 0.3.2
+* Fix bug when converting OneToOneRels.
+
 ## Version 0.3.1
 * Fix bug in `TimeDelta`
 
--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = '2019, Tormod Haugland'
 author = 'Tormod Haugland'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 # -- General configuration ---------------------------------------------------
 
