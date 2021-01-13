e9c86fdc3f5226471052a87eb697f6b6073b9ec5
Anton <anton.i.kulikov@gmail.com>
2021-01-13 12:40:21 -0800

    Add Batch Patch and Batch Update to exports.

--- graphene_django_cud/mutations/__init__.py
+++ graphene_django_cud/mutations/__init__.py
@@ -1,5 +1,7 @@
 from .batch_create import DjangoBatchCreateMutation
 from .batch_delete import DjangoBatchDeleteMutation
+from .batch_patch import DjangoBatchPatchMutation
+from .batch_update import DjangoBatchUpdateMutation
 from .filter_delete import DjangoFilterDeleteMutation
 from .create import DjangoCreateMutation
 from .delete import DjangoDeleteMutation
@@ -10,7 +12,9 @@ __all__ = (
     'DjangoCreateMutation',
     'DjangoBatchCreateMutation',
     'DjangoPatchMutation',
+    'DjangoBatchPatchMutation',
     'DjangoUpdateMutation',
+    'DjangoBatchUpdateMutation',
     'DjangoDeleteMutation',
     'DjangoBatchDeleteMutation',
     'DjangoFilterDeleteMutation',
