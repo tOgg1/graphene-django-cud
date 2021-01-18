5b2f9bb5fc6379a53abb6209a6fa570789724f4c
Tormod Haugland <tormod.haugland@gmail.com>
2021-01-18 05:10:15 +0100

    feat: Add DjangoFilterUpdateMutation to mutation exports

--- graphene_django_cud/mutations/__init__.py
+++ graphene_django_cud/mutations/__init__.py
@@ -3,19 +3,21 @@ from .batch_delete import DjangoBatchDeleteMutation
 from .batch_patch import DjangoBatchPatchMutation
 from .batch_update import DjangoBatchUpdateMutation
 from .filter_delete import DjangoFilterDeleteMutation
+from .filter_update import DjangoFilterUpdateMutation
 from .create import DjangoCreateMutation
 from .delete import DjangoDeleteMutation
 from .patch import DjangoPatchMutation
 from .update import DjangoUpdateMutation
 
 __all__ = (
-    'DjangoCreateMutation',
-    'DjangoBatchCreateMutation',
-    'DjangoPatchMutation',
-    'DjangoBatchPatchMutation',
-    'DjangoUpdateMutation',
-    'DjangoBatchUpdateMutation',
-    'DjangoDeleteMutation',
-    'DjangoBatchDeleteMutation',
-    'DjangoFilterDeleteMutation',
+    "DjangoCreateMutation",
+    "DjangoBatchCreateMutation",
+    "DjangoPatchMutation",
+    "DjangoBatchPatchMutation",
+    "DjangoUpdateMutation",
+    "DjangoBatchUpdateMutation",
+    "DjangoDeleteMutation",
+    "DjangoBatchDeleteMutation",
+    "DjangoFilterDeleteMutation",
+    "DjangoFilterUpdateMutation",
 )
